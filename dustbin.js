89278f7357604efd65914e122987eb22f33bdd9f
nehal1929 <74008636+nehal1929@users.noreply.github.com>
2020-11-28 23:57:22 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,78 @@
+class Dustbin
+{
+    constructor(x,y)
+    {
+        this.x=x
+        this.y=y
+        this.dustbinWidth=200;
+        this.dustbinHeight=100;
+        this.wallThickness=20;
+        this.angle=0;
+        this.bottomBody=Bodies.rectangle(this.x,this.y,this.dustbinWidth,this.wallThickness,{isStatic:true})
+        this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2,this.y-this.dustbinHeight/2,this.wallThickness,this.dustbinHeight,{isStatic:true})
+
+        Matter.Body.setAngle(this.leftWallBody,this.angle)
+        this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2,this.y-this.dustbinHeight/2,this.wallThickness,this.dustbinHeight,{isStatic:true})
+
+        Matter.Body.setAngle(this.rightWallBody,1*this.angle)
+
+        World.add(world, this.bottomBody);
+        World.add(world, this.rightWallBody);
+        World.add(world, this.leftWallBody);
+
+        
+        
+    }
+    
+    display(){
+        var posBottom = this.bottomBody.position;
+        var posLeft = this.rightWallBody.position;
+        var posRight = this.leftWallBody.position;
+        
+        push();
+        translate()
+        rectMode(CENTER)
+        angleMode(RADIUS)
+        fill(255)
+        stroke(255)
+        rotate(this.angle)
+        strokeWeight(3)
+        rect(0,0,this.wallThickness,this.dustbinHeight)
+        pop();
+
+
+
+
+        push();
+        translate()
+        rectMode(CENTER)
+        stroke(255)
+        angleMode(RADIUS)
+        fill(255)
+        rotate(1*this.angle)
+        rect(0,0,this.wallThickness,this.dustbinHeight)
+        pop();
+
+
+
+
+        push();
+        translate()
+        imageMode(CENTER)
+        stroke(255)
+        angleMode(RADIUS)
+        fill(255)
+        rotate(1*this.angle)
+        rect(0,0,this.wallThickness,this.dustbinHeight)
+        pop();
+
+ 
+    }
+
+
+
+
+
+
+
+}
